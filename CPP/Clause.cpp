2db9064a8207ebfd0a17cdf2332797db983494fe
Abhishek mutyampeta <abhi.abhishek594@gmail.com>
2017-09-18 20:06:59 -0500

    Update Clause.cpp

--- CPP/Clause.cpp
+++ CPP/Clause.cpp
@@ -1,7 +1,18 @@
+#include<iostream>
+using namespace std;
+class Clause1{
+public:
+int len;
+int* hgs;
+int base;
+int neck;
+int* xs;
+
+
 int Clause(const int len, int* hgs,const int base, const int neck, int* xs){
 this->hgs = hgs;
 this->base = base;
 this->len = len;
 this->neck = neck;
 this->xs = xs;
-}
+}};
