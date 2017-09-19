466ba575028842c7ffbe1b0d6781d0968db8eab5
Abhishek mutyampeta <abhi.abhishek594@gmail.com>
2017-09-18 19:55:47 -0500

    Delete Clause.cpp

--- CPP/Clause.cpp
+++
@@ -1,8 +0,0 @@
-using namespace CPP;
-int Clause(const int len, int* hgs,const int base, const int neck, int* xs){
-this->hgs = hgs;
-this->base = base;
- this->len = len;
-  this->neck = neck;
-  this->xs = xs;
-}
