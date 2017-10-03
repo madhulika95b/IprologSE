d0816cad3e8b511a99ac41d785d1c415f03da833
madhulika95b <32081283+madhulika95b@users.noreply.github.com>
2017-10-02 19:51:30 -0500

    Create IntList.cpp

---
+++ CPP/IntList.cpp
@@ -0,0 +1,17 @@
+
+Class IntList{
+private:
+  int head;
+  IntList tail;
+
+private:
+  void IntList(final int head) {
+    this.head = head;
+    tail = null;
+  }
+  void IntList(final int X, final IntList Xs) {
+    head = X;
+    tail = Xs;
+  }
+  
+}
