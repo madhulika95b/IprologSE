20b9fc1c74de71a540a2aa041601fe8939946637
Vishal Mangwani <mangwani.vishal@gmail.com>
2017-10-02 19:20:40 -0500

    Create Spine.cpp
    Spine.cpp is a conversion of Spine.java

---
+++ CPP/Spine.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+using namespace std;
+class Spine{
+public:
+
+Spine(int* gs0, int base, IntList gs, int ttop, int k, int* cs){
+hd=gs[0];
+this->base=base;
+this->gs=IntList.tail(IntList.app(gs0,gs));
+this->ttop=ttop;
+this->k=k;
+this->cs=cs;
+}
+
+
+}
