4c5b0fdf91f4c0fc8ade8abadb6a0032dec462b0
Abhishek mutyampeta <abhi.abhishek594@gmail.com>
2017-10-02 19:36:00 -0500

    Update Toks.cpp

--- CPP/Toks.cpp
+++ CPP/Toks.cpp
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+class Toks :: class StreamTokenizer{
+   static String IF = "if";
+   static String AND = "and";
+   static String DOT = ".";
+   static String HOLDS = "holds";
+   static String LISTS = "lists"; // todo
+   static String IS = "is"; 
+  
+  static Toks makeToks(String s, boolean fromFile) {
+  
+    try{
+      fopen R;
+    
+  }
+};
